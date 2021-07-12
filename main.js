bd0768220ee6175d6b401e20919b973b90953f38
hhghghgjdjwi <87009218+hhghghgjdjwi@users.noreply.github.com>
2021-07-12 18:34:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+noseX=0;
+noseY=0;
+function preload(){
+
+} 
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center(); 
+}
+function draw()
+{
+
+}
+function take_snapshot()
+{
+    save('Catispro.png');
+    
+}
